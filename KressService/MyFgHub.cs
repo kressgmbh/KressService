f2df009c83397cf4fa4f5ac3a5dd02defaf139cc
Thomas Kling <kling.thomas@t-online.de>
2017-11-24 11:53:48 +0100

    Projektdateien hinzufügen.

---
+++ KressService/MyFgHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace KressService
+{
+    public class MyFgHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void Send( string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage( message);
+        }
+    }
+}
